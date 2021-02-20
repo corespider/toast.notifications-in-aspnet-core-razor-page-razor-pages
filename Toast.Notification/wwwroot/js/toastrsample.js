7a3f417a56775967298fcb800dfd69f6641395b1
Your Name <corespiderpro@gmail.com>
2021-02-21 02:25:43 +0530

    Add project files.

---
+++ Toast.Notification/wwwroot/js/toastrsample.js
@@ -0,0 +1,17 @@
+﻿
+$(function(){
+$('#showtoast').click(function () {
+        toastr.options = {
+        "debug": false,
+        "positionClass": $("#toastrPositionGroup input:radio:checked").val(),
+        "onclick": null,
+        "fadeIn": 300,
+        "fadeOut": 100,
+        "timeOut": 3000,
+        "extendedTimeOut": 1000
+        }
+        
+        var d= Date();
+        toastr[$("#toastrTypeGroup input:radio:checked").val()](d,"Current Day & Time");  
+});
+});
